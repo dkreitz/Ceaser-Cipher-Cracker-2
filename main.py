90a17577fb342918d33071ea4ac283a6433dd35f
dkreitz <dkreitz@users.noreply.github.com>
2023-11-11 23:39:01 +0000

    just want to commit before I start making changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Define the frequency of letters in the English language
 englishLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 
                      'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C': 2.78, 
                      'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97, 
